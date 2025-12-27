421e4f6e6a479bf12c3c1656c3e64b0b5739e8ea
Hunter Dyar <bloop@hdyar.com>
2025-12-26 22:00:59 -0500

    student details view

--- ProductionManager/Views/StudentView/StudentDetailsView.cs
+++ ProductionManager/Views/StudentView/StudentDetailsView.cs
@@ -1,26 +1,85 @@
+using System.Diagnostics;
+using Eto.Drawing;
 using Eto.Forms;
 
 namespace ProductionManager.Views;
 
-public class StudentDetailsView(MainWindow mainWindow) : GroupBox
+public class StudentDetailsView : GroupBox
 {
-    private MainWindow _mainWindow = mainWindow;
+    private MainWindow _mainWindow;
+    private ToggleButton _weekToggle;
+    private StudentWeek _studentWeek;
+    private StudentWeekView _studentWeekView;
+
+    public StudentDetailsView(MainWindow mainWindow)
+    {
+        _mainWindow = mainWindow;
+        _weekToggle = new ToggleButton();
+        _weekToggle.Text = "Show Grades";
+        _weekToggle.Checked = false;
+        _weekToggle.CheckedChanged += WeekToggleOnCheckedChanged;
+    }
+
+    private void WeekToggleOnCheckedChanged(object? sender, EventArgs e)
+    {
+        _studentWeekView.Visible = _weekToggle.Checked;
+    }
+
     public Student? CurrentStudent { get; private set; }
 
     public void SetStudent(Student? student)
     {
         CurrentStudent = student;
-        if (CurrentStudent != null)
-        {
-            Content = new Label()
-            {
-                Text = CurrentStudent?.FirstName,
-            };
-        }
-        else
-        {
-            Content = null;
-        }
+        _studentWeek = new StudentWeek(student, _mainWindow.DataStore);
+        
+        _studentWeekView = new StudentWeekView(_studentWeek);
+       // _studentWeekView.MinimumSize = new Size(this.Width-100, 120);
+        _studentWeekView.Width = this.Width;
+        _studentWeekView.Height = 50;
+        _studentWeekView.Visible = _weekToggle.Checked;
+        
+        var layout = new DynamicLayout();
+        layout.Spacing = new Size(4,4);
+        layout.BeginVertical();
+        //headers. matches below.
+        layout.BeginCentered();
+
+        layout.BeginGroup("information");
+        
+        layout.BeginHorizontal();
+        layout.Add(GetText("First"));
+        layout.Add(GetText("Last"));
+        layout.Add(GetText("Section"));
+        layout.Add(GetText("Level"));
+        layout.Add(null);
+        layout.Add(_weekToggle);
+        layout.EndHorizontal();
+        layout.BeginHorizontal();
+        layout.Add(GetText(CurrentStudent.FirstName));
+        layout.Add(GetText(CurrentStudent.LastName));
+        layout.Add(GetText(CurrentStudent.Section.ToString()));
+        layout.Add(GetText(CurrentStudent.ClassLevel.ToString()));
+
+        layout.EndHorizontal();
+        
+        layout.EndGroup();
+        layout.EndCentered();
+        //
+        layout.Add(null);
+        layout.BeginGroup("Grades");
+        layout.AddSeparateRow(_studentWeekView);
+        layout.EndGroup();
+        layout.EndVertical();
+        
+       
+        Content = layout;
     }
-    
+
+    private Control GetText(string t)
+    {
+        return new Label()
+        {
+            Text = t,
+        };
+    }
 }
